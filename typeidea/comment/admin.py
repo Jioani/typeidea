7845d36d15c7a59cc86697f0f4513839d279448a
jijinshan <jijinshan@qq.com>
2021-12-27 16:01:51 +0800

    管理端配置

--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from comment.models import Comment
+from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
+
+
+@admin.register(Comment, site=custom_site)
+class CommentAdmin(BaseOwnerAdmin):
+    list_display = ('target', 'content', 'nickname', 'website', 'email', 'status', 'create_time')
+    list_filter = ('status',)
+    search_fields = ('content', 'nickname', 'email')
+
+    save_on_top = True
+    actions_on_top = True
+
+    fields = (
+        ('target', 'website'),
+        ('nickname', 'email'),
+        'content',
+        'status'
+    )
--- typeidea/config/admin.py
+++ typeidea/config/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from config.models import Link, SideBar
+from typeidea.base_admin import BaseOwnerAdmin
+from typeidea.custom_site import custom_site
+
+
+@admin.register(Link, site=custom_site)
+class LinkAdmin(BaseOwnerAdmin):
+    list_display = ('title', 'href', 'owner', 'status', 'create_time')
+    list_filter = ('status',)
+    search_fields = ('title', 'owner')
+
+
+@admin.register(SideBar)
+class SideBarAdmin(BaseOwnerAdmin):
+    list_display = ('title', 'display_type', 'owner', 'status', 'create_time')
+    list_filter = ('display_type', 'status')
+    search_fields = ('title', 'owner')
+
